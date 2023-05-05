37f1996d8c95412876a1d406c4caa11223a17e69
KevserTemur <kevsertemur@gmail.com>
2023-05-05 20:55:19 +0300

    appium trendyol

--- src/test/java/stepdefinitions/FilterPageSteps.java
+++ src/test/java/stepdefinitions/FilterPageSteps.java
@@ -6,6 +6,7 @@ import org.testng.Assert;
 import pages.FilterPage;
 import utils.DriverManager;
 
+
 public class FilterPageSteps {
     FilterPage filterPage = new FilterPage(DriverManager.getDriver());
 
